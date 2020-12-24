204ad24fe51ef965f8ff9febc0acbd0f5fd09bb8
Harry Gothold <harrygot@hotmail.com>
2020-12-24 15:21:27 +0000

    Initial commit

---
+++ lambda/todoHandler.ts
@@ -0,0 +1,102 @@
+import AWS = require('aws-sdk');
+import { v4 as uuid } from 'uuid';
+// the table name that we get from an env variable
+const tableName = process.env.TABLE_NAME || '';
+// for interacting with dynamoDB from JavaScript / nodeJS
+const dynamo = new AWS.DynamoDB.DocumentClient();
+
+const createResponse = (
+  body: string | AWS.DynamoDB.DocumentClient.ItemList,
+  statusCode = 200
+) => {
+  return {
+    statusCode,
+    headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "OPTIONS,GET,POST,DELETE"
+    },
+    body: JSON.stringify(body, null, 2),
+  };
+};
+// DynamoDB Scan operation scans and returns all of the items in the db
+const getAllTodos = async () => {
+  const scanResult = await dynamo
+    .scan({
+      TableName: tableName,
+    })
+    .promise();
+
+  return scanResult;
+};
+
+const addTodoItem = async (data: { todo: string; id: string }) => {
+  const { id, todo } = data;
+  if (todo && todo !== '') {
+    await dynamo.put({
+      TableName: tableName,
+      Item: {
+        id: id || uuid(),
+        todo,
+      },
+    }).promise();
+  }
+  return todo;
+};
+
+const deleteTodoItem = async (data: {id: string }) => {
+    const { id } = data;
+
+    if (id && id !== '') {
+        await dynamo.delete({
+            TableName: tableName,
+            Key: { id }
+        }).promise();
+    }
+    return id;
+}
+
+// async function that respons to apiGateway events
+exports.handler = async function (event: AWSLambda.APIGatewayEvent) {
+    try {
+        const { httpMethod, body: requestBody } = event;
+        if (httpMethod === "OPTIONS") {
+            return createResponse('Ok');
+        }
+        // if GET return todos
+        if (httpMethod === "GET") {
+            const response = await getAllTodos();
+
+            return createResponse(response.Items || []);
+        }
+        if (!requestBody) {
+            return createResponse("Missing request body", 500);
+        }
+
+        // parsing the data we sent to the server
+        const data = JSON.parse(requestBody);
+        // if POST add a todo
+        if (httpMethod === "POST") {
+            const todo = await addTodoItem(data);
+            return todo
+                ? createResponse(`${todo} added to the database`)
+                : createResponse("Todo is missing", 500);
+        }
+        // if DELETE, delete todo (we'll imlement that in the next lesson)
+        if (httpMethod === "DELETE") {
+            const id = await deleteTodoItem(data);
+            return id
+                ? createResponse(
+                      `Todo item with an id of ${id} deleted from the database`
+                  )
+                : createResponse("ID is missing", 500);
+        }
+
+        return createResponse(
+            `We only accept GET, POST, OPTIONS and DELETE, not ${httpMethod}`,
+            500
+        );
+    } catch (error) {
+        console.log(error);
+        return createResponse(error, 500);
+    }
+};
---
+++ lib/todo-app-stack.ts
@@ -0,0 +1,50 @@
+import * as cdk from '@aws-cdk/core';
+import * as apiGateway from '@aws-cdk/aws-apigateway';
+import * as s3 from '@aws-cdk/aws-s3';
+import * as s3Deployment from '@aws-cdk/aws-s3-deployment';
+import { TodoBackend } from './todo-backend';
+import { SPADeploy } from 'cdk-spa-deploy';
+
+export class TodoAppStack extends cdk.Stack {
+  constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    const todoBackend = new TodoBackend(this, "TodoBackend");
+
+    new apiGateway.LambdaRestApi(this, 'Endpoint', {
+      handler: todoBackend.handler,
+    });
+
+    const logoBucket = new s3.Bucket(this, 'LogoBucket', {
+      publicReadAccess: true
+    });
+
+    new s3Deployment.BucketDeployment(this, 'DeployLogo', {
+      destinationBucket: logoBucket,
+      sources: [s3Deployment.Source.asset('./assets')]
+    });
+
+    new cdk.CfnOutput(this, "LogoPath", {
+      value: `https://${logoBucket.bucketDomainName}/logo.png`
+    });
+
+    // const websiteBucket = new s3.Bucket(this, "WebsiteBucket", {
+    //   publicReadAccess: true,
+    //   websiteIndexDocument: 'index.html'
+    // });
+
+    // new s3Deployment.BucketDeployment(this, 'DeployWebsite', {
+    //   destinationBucket: websiteBucket,
+    //   sources: [s3Deployment.Source.asset('../frontend/build')]
+    // });
+
+    // new cdk.CfnOutput(this, "WebsiteAddress", {
+    //   value: websiteBucket.bucketWebsiteUrl
+    // });
+
+    new SPADeploy(this, "WebsiteDeploy").createSiteWithCloudfront({
+      indexDoc: "index.html",
+      websiteFolder: "../frontend/build"
+    });
+  }
+}
